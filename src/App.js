111f2b11762f43c900e7a8a0e2c15aad31092d13
MBJ-97 <mehdiibeldjoudi@gmail.com>
2021-11-29 09:39:41 +0100

    tools chap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-const Pet = (props) => {
-    return React.createElement('div', {}, [
-        React.createElement('h2', {}, props.name),
-        React.createElement('h3', {}, props.animal),
-        React.createElement('h3', {}, props.breed)  
-    ])
-}
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
-    return React.createElement(
-        'div',
-        {},
-        [ // Array here is optional we can take it off
-            // the react create element can be deleted here we can see how useful JSX is
-            React.createElement('h1', {}, 'Adopt me'),
-            React.createElement(Pet, {name:'Luna', animal:'dog', breed:'havanese'}),
-            React.createElement(Pet, {name:'Titi', animal:'bird', breed:'canari'}),
-            React.createElement(Pet, {name:'Lisa', animal:'cat', breed:'siamois'})
-
-        ]
-        );
+  return React.createElement("div", {}, [
+    // Array here is optional we can take it off
+    // the react create element can be deleted here we can see how useful JSX is
+    React.createElement("h1", {}, "Adopt me"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "dog",
+      breed: "havanese",
+    }),
+    React.createElement(Pet, { name: "Titi", animal: "bird", breed: "canari" }),
+    React.createElement(Pet, { name: "Lisa", animal: "cat", breed: "siamois" }),
+  ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
